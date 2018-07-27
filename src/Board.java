1d5282989b42afc4f1ac8f8bc9b0ae18c31473d9
alnbx <alonboxiner@gmail.com>
2018-07-27 18:56:18 +0300

    Building the board as part of the game engine

---
+++ src/Board.java
@@ -0,0 +1,80 @@
+/**
+ * Created by user on 27/07/2018.
+ */
+public class Board {
+
+    private int rows;
+    private int cols;
+    private Col[] board;
+    private boolean hasWinner;
+    private int winningPlayer;
+
+    public Board(int rows, int cols)
+    {
+        this.rows = rows;
+        this.cols = cols;
+        board = new Col[cols];
+        this.hasWinner = false;
+        this.winningPlayer = 0;
+
+        initBoard();
+    }
+
+    private void initBoard()
+    {
+        for (int i = 0; i < this.cols; i++) { board[i] = new Col(i, this.rows); }
+
+        connectBoard();
+    }
+
+    private void connectBoard()
+    {
+        for (int i = 0; i < this.cols; i++)
+        {
+            connectDiscsToLeft(i);
+            connectDiscsToRight(i);
+            connectDiscsToDownUp(i);
+        }
+    }
+
+    private void connectDiscsToLeft(int loc)
+    {
+        if (loc != 0)
+        {
+            board[loc].connectDiscsLeft(board[loc - 1]);
+            board[loc].connectDiscsLeftDown(board[loc - 1]);
+            board[loc].connectDiscsLeftUp(board[loc - 1]);
+        }
+    }
+
+    private void connectDiscsToRight(int loc)
+    {
+        if (loc != this.cols - 1)
+        {
+            board[loc].connectDiscsRight(board[loc + 1]);
+            board[loc].connectDiscsRightUp(board[loc + 1]);
+            board[loc].connectDiscsRightDown(board[loc + 1]);
+        }
+    }
+
+    private void connectDiscsToDownUp(int loc)
+    {
+        board[loc].connectDiscsDown();
+        board[loc].connectDiscsUp();
+    }
+
+    //TODO: check if necessary! Delete it if not!
+    public Col[] getBoard() { return this.board; }
+    public void setWinner(int winningPlayer) { this.hasWinner = true; this.winningPlayer = winningPlayer; }
+    public boolean isHasWinner() { return this.hasWinner; }
+    public int getWinner() { return this.winningPlayer; }
+
+    static public void main(String[] args)
+    {
+        int x = 4;
+        int y = 5;
+        Board b = new Board(x, y);
+
+        x = 6;
+    }
+}
---
+++ src/Directions.java
@@ -0,0 +1,6 @@
+/**
+ * Created by user on 27/07/2018.
+ */
+public enum Directions {
+    LEFT, LEFTUP, UP, UPRIGHT, RIGHT, RIGHTDOWN, DOWN, LEFTDOWN;
+}
